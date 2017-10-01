d0560694ea73238b1a3853bb43eb98faa33a93f0
Rob Bayliss <rob@lastcallmedia.com>
2017-10-01 12:16:02 -0400

    Also install extra files on package update

--- src/Plugin.php
+++ src/Plugin.php
@@ -36,6 +36,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     {
         return [
             PackageEvents::POST_PACKAGE_INSTALL => 'installExtraFiles',
+            PackageEvents::POST_PACKAGE_UPDATE => 'installExtraFiles'
         ];
     }
 

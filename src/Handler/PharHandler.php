000835cba4667b559a29002bae31dc196041b1d1
tienvx <tien.xuan.vo@gmail.com>
2022-11-24 21:30:41 +0700

    Make phar executable

--- src/Handler/PharHandler.php
+++ src/Handler/PharHandler.php
@@ -3,24 +3,21 @@
 namespace LastCall\DownloadsPlugin\Handler;
 
 use Composer\Composer;
-use Composer\Installer\BinaryInstaller;
 use Composer\IO\IOInterface;
-use LastCall\DownloadsPlugin\Subpackage;
+use Composer\Util\Platform;
+use Composer\Util\ProcessExecutor;
 
 class PharHandler extends FileHandler
 {
-    protected function createSubpackage(): Subpackage
-    {
-        $pkg = parent::createSubpackage();
-        $pkg->setBinaries([$this->extraFile['path']]);
-
-        return $pkg;
-    }
-
     public function download(Composer $composer, IOInterface $io): void
     {
         parent::download($composer, $io);
-        $binaryInstaller = new BinaryInstaller($io, \dirname($this->getTargetPath()), 'auto');
-        $binaryInstaller->installBinaries($this->getSubpackage(), $this->parentPath);
+
+        if (Platform::isWindows()) {
+            $proxy = $this->getTargetPath().'.bat';
+            file_put_contents($proxy, '@php "%~dp0'.ProcessExecutor::escape(basename($proxy, '.bat')).'" %*');
+        } else {
+            chmod($this->getTargetPath(), 0777 ^ umask());
+        }
     }
 }

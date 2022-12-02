663ccba98483e0966e45d6aebbad0814f368f595
tienvx <tien.xuan.vo@gmail.com>
2022-12-02 12:43:38 +0700

    Test file handler

--- tests/Unit/Handler/BaseHandlerTestCase.php
+++ tests/Unit/Handler/BaseHandlerTestCase.php
@@ -7,30 +7,26 @@ use Composer\Downloader\DownloadManager;
 use Composer\IO\IOInterface;
 use Composer\Package\PackageInterface;
 use Composer\Package\RootPackage;
-use Composer\Util\Loop;
 use LastCall\DownloadsPlugin\BinariesInstaller;
-use LastCall\DownloadsPlugin\GlobCleaner;
 use LastCall\DownloadsPlugin\Handler\HandlerInterface;
 use LastCall\DownloadsPlugin\Subpackage;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use React\Promise\PromiseInterface;
 
 abstract class BaseHandlerTestCase extends TestCase
 {
-    private Composer|MockObject $composer;
+    protected Composer|MockObject $composer;
     private IOInterface|MockObject $io;
-    private DownloadManager|MockObject $downloadManager;
-    private GlobCleaner|MockObject $cleaner;
+    protected DownloadManager|MockObject $downloadManager;
     private BinariesInstaller|MockObject $binariesInstaller;
     protected PackageInterface|MockObject $parent;
     protected string $parentPath = '/path/to/package';
     protected string $id = 'sub-package-name';
-    private string $url = 'http://example.com/file.ext';
-    private string $path = 'files/file';
+    protected string $url = 'http://example.com/file.ext';
+    protected string $path = 'files/file';
     protected array $extraFile;
     protected string $targetPath;
-    private array $ignore = ['file.*', '!file.ext'];
+    protected array $ignore = ['file.*', '!file.ext'];
     protected string $parentName = 'vendor/parent-package';
 
     protected function setUp(): void
@@ -38,7 +34,6 @@ abstract class BaseHandlerTestCase extends TestCase
         $this->composer = $this->createMock(Composer::class);
         $this->io = $this->createMock(IOInterface::class);
         $this->downloadManager = $this->createMock(DownloadManager::class);
-        $this->cleaner = $this->createMock(GlobCleaner::class);
         $this->binariesInstaller = $this->createMock(BinariesInstaller::class);
         $this->parent = $this->createMock(PackageInterface::class);
         $this->extraFile = [
@@ -99,7 +94,7 @@ abstract class BaseHandlerTestCase extends TestCase
         $this->parent->expects($this->exactly(2))->method('getName')->willReturn($this->parentName);
         $handler = $this->createHandler($this->parent, $this->parentPath, $this->extraFile + ['executable' => $executable]);
         $this->expectException(\UnexpectedValueException::class);
-        $this->expectExceptionMessage(sprintf('Attribute "executable" of extra file "%s" defined in package "%s" must be array, "%s" given.', $this->id, $this->parentName, $type));
+        $this->expectExceptionMessage(sprintf('Attribute "executable" of extra file "%s" defined in package "%s" must be '.$this->getExecutableType().', "%s" given.', $this->id, $this->parentName, $type));
         $handler->getSubpackage();
     }
 
@@ -107,12 +102,7 @@ abstract class BaseHandlerTestCase extends TestCase
     {
         $this->parent->expects($this->once())->method('getName')->willReturn($this->parentName);
         $handler = $this->createHandler($this->parent, $this->parentPath, $this->extraFile + ['ignore' => $this->ignore]);
-        $this->assertSame([
-            'ignore' => $this->ignore,
-            'name' => "{$this->parentName}:{$this->id}",
-            'url' => $this->url,
-            'checksum' => $this->getChecksum(),
-        ], $handler->getTrackingData());
+        $this->assertSame($this->getTrackingData(), $handler->getTrackingData());
     }
 
     public function testGetChecksum(): void
@@ -135,60 +125,35 @@ abstract class BaseHandlerTestCase extends TestCase
 
     public function testInstall(): void
     {
-        $this->composer->expects($this->once())->method('getDownloadManager')->willReturn($this->downloadManager);
-        $isComposerV2 = version_compare(Composer::RUNTIME_API_VERSION, '2.0.0') >= 0;
-        if ($isComposerV2) {
-            $downloadPromise = $this->createMock(PromiseInterface::class);
-            $installPromise = $this->createMock(PromiseInterface::class);
-            $this->downloadManager
-                ->expects($this->once())
-                ->method('download')
-                ->with($this->isInstanceOf(Subpackage::class), $this->targetPath)
-                ->willReturn($downloadPromise);
-            $this->downloadManager
-                ->expects($this->once())
-                ->method('install')
-                ->with($this->isInstanceOf(Subpackage::class), $this->targetPath)
-                ->willReturn($installPromise);
-            $loop = $this->createMock(Loop::class);
-            $loop
-                ->expects($this->exactly(2))
-                ->method('wait')
-                ->withConsecutive(
-                    [[$downloadPromise]],
-                    [[$installPromise]]
-                );
-            $this->composer->expects($this->exactly(2))->method('getLoop')->willReturn($loop);
-        } else {
-            $this->downloadManager
-                ->expects($this->once())
-                ->method('download')
-                ->with($this->isInstanceOf(Subpackage::class), $this->targetPath);
-        }
+        $this->assertDownload();
         $this->binariesInstaller
             ->expects($this->once())
             ->method('install')
             ->with($this->isInstanceOf(Subpackage::class), $this->parentPath, $this->io);
-        $this->cleaner->expects($this->once())->method('clean')->with($this->targetPath, $this->ignore);
         $handler = $this->createHandler($this->parent, $this->parentPath, $this->extraFile + ['ignore' => $this->ignore]);
         $handler->install($this->composer, $this->io);
     }
 
+    abstract protected function assertDownload(): void;
+
     abstract protected function getHandlerClass(): string;
+
+    abstract protected function getHandlerExtraArguments(): array;
 
     abstract protected function getTrackingFile(): string;
 
-    protected function getDistType(): string
-    {
-        return 'file';
-    }
+    abstract protected function getDistType(): string;
 
     abstract protected function getChecksum(): string;
+
+    abstract protected function getExecutableType(): string;
+
+    abstract protected function getTrackingData(): array;
 
     protected function createHandler(PackageInterface $parent, string $parentPath, array $extraFile): HandlerInterface
     {
         $class = $this->getHandlerClass();
 
-        return new $class($parent, $parentPath, $extraFile, $this->binariesInstaller, $this->cleaner);
+        return new $class($parent, $parentPath, $extraFile, $this->binariesInstaller, ...$this->getHandlerExtraArguments());
     }
 }
